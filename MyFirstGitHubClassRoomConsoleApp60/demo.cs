83fe4582ceb5e5ff429106ff9f17564b9e8cf099
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2022-09-05 12:31:56 +0200

    Made properties and instance fields

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+        private int _tal;
+        private string _name;
+        private string _description;
+
+        public int Tal
+        {
+            get { return _tal; }
+            set { _tal = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+    }
+}
